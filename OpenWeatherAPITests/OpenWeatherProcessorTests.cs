ed7488859eca54dcad41f1d8dc013ee095b559c1
maxime <maxime.milot@hotmail.ca>
2021-11-01 19:05:15 -0400

    T3 - Complété 1962320

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -11,7 +11,7 @@ namespace OpenWeatherAPITests
         public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
         {
             OpenWeatherProcessor openWeatherProcessor = OpenWeatherProcessor.Instance;
-
+            ApiHelper.InitializeClient();
 
             Assert.ThrowsAsync<ArgumentException>(openWeatherProcessor.GetOneCallAsync);
         }
@@ -20,9 +20,23 @@ namespace OpenWeatherAPITests
         public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
         {
             OpenWeatherProcessor openWeatherProcessor = OpenWeatherProcessor.Instance;
-
+            ApiHelper.InitializeClient();
 
             Assert.ThrowsAsync<ArgumentException>(openWeatherProcessor.GetCurrentWeatherAsync);
+        }
+
+        [Fact]
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            OpenWeatherProcessor openWeatherProcessor = OpenWeatherProcessor.Instance;
+
+            Assert.ThrowsAsync<ArgumentException>(openWeatherProcessor.GetOneCallAsync);
+        }
+
+        [Fact]
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            OpenWeatherProcessor openWeatherProcessor = OpenWeatherProcessor.Instance;
         }
     }
 }
