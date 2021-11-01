e2169bbdfe20b15682de5f5679c512da493a50cd
maxime <maxime.milot@hotmail.ca>
2021-11-01 19:58:43 -0400

    AppConfiguration complété - 1962320

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+using System.Configuration;
+
+namespace WeatherApp
+{
+    public static class AppConfiguration
+    {
+        private static IConfiguration configuration;
+
+        public static string GetValue(string str)
+        {
+            if(configuration == null)
+            {
+                InitConfig();
+            }
+            return configuration[str];
+        }
+
+        private static void InitConfig()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddJsonFile("appsettings.json",
+            optional: true,
+            reloadOnChange: true);
+
+            builder.AddUserSecrets<MainWindow>();
+
+            configuration = builder.Build();
+        }
+    }
+}
