1a3addd892cef2f9c37a03a1212beed6fc0036cd
maxime <maxime.milot@hotmail.ca>
2021-11-01 20:27:27 -0400

    OpenWeatherService Complété - 1962320

---
+++ WeatherApp/OpenWeatherService.cs
@@ -0,0 +1,34 @@
+﻿using OpenWeatherAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.Services;
+
+namespace WeatherApp
+{
+    public class OpenWeatherService : IWindDataService
+    {
+        private OpenWeatherProcessor owp;
+        public WindDataModel LastWindData;
+        public OpenWeatherService(string apiKey)
+        {
+            owp = OpenWeatherProcessor.Instance;
+            owp.ApiKey = apiKey;
+        }
+
+        public async Task<WindDataModel> GetDataAsync()
+        {
+            var temp = await owp.GetCurrentWeatherAsync();
+            var result = new WindDataModel
+            {
+                DateTime = DateTime.UnixEpoch.AddSeconds(temp.DateTime),
+                Direction = temp.Wind.Deg
+            };
+            LastWindData = result;
+            return LastWindData;
+        }
+    }
+}
